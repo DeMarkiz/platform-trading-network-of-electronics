793ddb8ffe16e447f0a8c58fa6fc0b52182afde0
DeMarkiz <markberezhnoy27@gmail.com>
2025-04-16 17:00:04 +0300

    add permission

--- network/views.py
+++ network/views.py
@@ -2,10 +2,12 @@ from rest_framework import viewsets
 from .models import NetworkNode
 from .serializers import NetworkNodeSerializer
 from django_filters.rest_framework import DjangoFilterBackend
+from .permissions import IsActiveStaff
 
 
 class NetworkNodeViewSet(viewsets.ModelViewSet):
     queryset = NetworkNode.objects.all()
     serializer_class = NetworkNodeSerializer
+    permission_classes = [IsActiveStaff]
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['country', 'city', 'type', 'supplier']
